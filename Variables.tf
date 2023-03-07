06e6402b244d01947e3ca407469c3bb02da5c3db
Shrook Mostafa <shrookmostafa689@gmail.com>
2023-03-07 14:49:59 +0200

    Update Variables.tf

--- Variables.tf
+++ Variables.tf
@@ -2,21 +2,11 @@ variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
 
-variable "enable_dns_support" {
-    default = "true"
-}
 
 variable "enable_dns_hostnames" {
     default ="true" 
 }
 
-variable "enable_classiclink" {
-    default = "false"
-}
-
-variable "enable_classiclink_dns_support" {
-    default = "false"
-}
 
 variable "preferred_number_of_public_subnets" {
       default = null
@@ -31,10 +21,26 @@ variable "public_route_table_cidr" {
 
 }
 
+variable "preferred_number_of_elastic_ip" {
+  default = null
+}
+
+variable "preferred_number_of_nat"{
+  default = null
+}
+
+variable "preferred_number_of_private_subnets_association" {
+  default = null
+}
+
 variable "ip_address_type" {
   default = "string"
 }
 
 variable "load_balancer_type" {
+  default = "string"
+}
+
+variable "keypair" {
   default = "string"
 }
