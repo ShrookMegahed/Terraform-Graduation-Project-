dee29c70c9c62ae4b1cc85ddb9e98dacae2cdfbe
Shrook Mostafa <shrookmostafa689@gmail.com>
2023-03-07 14:16:36 +0200

    Update natgateway.tf

--- natgateway.tf
+++ natgateway.tf
@@ -1,19 +1,21 @@
-/* resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat-eip.id
-  subnet_id     = element(aws_subnet.public.*.id, 0)
-  depends_on    = [aws_internet_gateway.gw]
+# resource "aws_nat_gateway" "nat" {
+#   count = var.preferred_number_of_nat
+#    allocation_id = aws_eip.nat-eip[count.index].id
+#    subnet_id     = element(aws_subnet.public.*.id, count.index)
 
-  tags = {
-    Name = "nat_gateway"
-  }
-}
+#   tags = {
+#      "Name" = format("nat-%d",count.index)
+#    }
+#   depends_on = [aws_internet_gateway.gw]
+#  }
 
-resource "aws_eip" "nat-eip" {
-  depends_on = [
-    aws_internet_gateway.gw
-  ]
-  vpc = true
-  tags = {
-    "name" = "nat-eip"
-  }
-} */
+
+# resource "aws_eip" "nat-eip" {
+#   depends_on = [
+#     aws_internet_gateway.gw
+#   ]
+#   vpc = true
+#   tags = {
+#     "name" = format("nat-eip-%d",count.index)
+#   }
+# }
