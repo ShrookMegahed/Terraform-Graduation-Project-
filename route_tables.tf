cab21fc3c710d4806588b2549e8730d908a9d976
Shrook Mostafa <shrookmostafa689@gmail.com>
2023-03-07 14:57:25 +0200

    Update route_tables.tf

--- route_tables.tf
+++ route_tables.tf
@@ -8,7 +8,7 @@ resource "aws_route_table" "private-rtb" {
 # associate all private subnets to the private route table
 resource "aws_route_table_association" "private-subnets-assoc" {
   count          = length(aws_subnet.private[*].id)
-  subnet_id      = element(aws_subnet.private[*].id, count.index)
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private-rtb.id
 }
 
@@ -27,7 +27,7 @@ resource "aws_route_table" "public-rtb" {
 # associate all public subnets to the public route table
 resource "aws_route_table_association" "public-subnets-assoc" {
   count          = length(aws_subnet.public[*].id)
-  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public-rtb.id
 }
 
